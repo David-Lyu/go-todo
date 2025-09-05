2328ad4e6bca854a117b7c3c1d75edf922ecd63e
David-Lyu <lyu.david@yahoo.com>
2025-09-04 20:06:39 -0700

    added functions to todo menu

---
+++ internal/menu/todo.go
@@ -0,0 +1,31 @@
+package menu
+
+type TodoMenu struct {
+	UserChoice int8
+	UserInput  string
+}
+
+// The acutal menu
+func (tM *TodoMenu) TodoMenu() {
+
+}
+
+// The help functions
+func (tM *TodoMenu) AddTodo() {
+	//Menu for adding
+	// Need the array of todos.
+
+}
+
+func (tM *TodoMenu) RemoveTodo() {
+
+}
+
+func (tm *TodoMenu) EditTodo() {
+
+}
+
+// This is a menu that can add sub tasks or complete todo
+func (tM *TodoMenu) SelectTodo() {
+
+}
